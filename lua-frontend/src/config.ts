a6375609c34183e39ea29ab6367562779bf73e54
tunght91 <hoangtung.utc@gmail.com>
2020-09-30 02:20:12 +0700

    update rpc

--- lua-frontend/src/config.ts
+++ lua-frontend/src/config.ts
@@ -1,5 +1,7 @@
 export default {
-  rpc: process.env.NODE_ENV === 'production' ? 'https://mainnet.infura.io/v3/588a6747988443e18df8680e9b69f9c1' : 'https://mainnet.eth.aragon.network/', //'https://main-rpc.linkpool.io/',
+  rpc: process.env.NODE_ENV === 'production' ? 
+    'https://wallet.tomochain.com/api/luaswap/rpc' :
+    'https://mainnet.eth.aragon.network/', //'https://main-rpc.linkpool.io/',
   chainId: 1,
   api: 'https://wallet.tomochain.com'
 }
