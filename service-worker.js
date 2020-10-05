419e0ce475436ba1565310b2aadbf2d13535564b
tunght91 <hoangtung.utc@gmail.com>
2020-10-05 17:30:24 +0700

    add new pool

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b82c3f6c85c9e0f1813c09423a485d4.js"
+  "/precache-manifest.ec46db8904111ce5d4c93ad77c84a4ed.js"
 );
 
 self.addEventListener('message', (event) => {
