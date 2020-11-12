4d28dc704776b7139a6e87e0aba4b191bdf274bf
tunght91 <hoangtung.utc@gmail.com>
2020-11-12 10:19:51 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,5 @@
 export default {
-  rpc: process.env.NODE_ENV === 'production' ? 
-    'https://wallet.tomochain.com/api/luaswap/rpc' :
-    'http://localhost:8020/rpc',
+  rpc: 'https://wallet.tomochain.com/api/luaswap/rpc',
   chainId: 1,
-  api: process.env.NODE_ENV === 'production' ? 
-  'https://wallet.tomochain.com/api/luaswap' :
-  'http://localhost:8020'
+  api: 'https://wallet.tomochain.com/api/luaswap'
 }
