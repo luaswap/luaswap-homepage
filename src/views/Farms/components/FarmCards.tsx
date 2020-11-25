1f1f5b8556ab966ba23858de4a88f1e65836b667
tunght91 <hoangtung.utc@gmail.com>
2020-11-25 14:36:59 +0700

    show tlv

--- src/views/Farms/components/FarmCards.tsx
+++ src/views/Farms/components/FarmCards.tsx
@@ -154,15 +154,15 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm }) => {
               )}
             </Button>
             <br/>
+            <StyledInsight>
+              <span>Total Locked Value</span>
+              <span>
+                {farm.usdValue &&
+                  <><b>{parseFloat(farm.usdValue.toFixed(0)).toLocaleString('en-US')} USD</b></>
+                }
+              </span>
+            </StyledInsight>
             {!farm.isHot && <>
-              <StyledInsight>
-                <span>Total Locked Value</span>
-                <span>
-                  {farm.usdValue &&
-                    <><b>{parseFloat(farm.usdValue.toFixed(0)).toLocaleString('en-US')} USD</b></>
-                  }
-                </span>
-              </StyledInsight>
               <StyledInsight>
                 <span>Reward</span>
                 <span>
