eb92319d1d6f5a52dc7e54bf7c501a7b9ec1a1d8
tunght91 <hoangtung.utc@gmail.com>
2020-11-25 14:38:51 +0700

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.03b76e2eed025e88bea910c78daaf786.js"
+  "/precache-manifest.1068d43f1942cf257583d5239c8cc12b.js"
 );
 
 self.addEventListener('message', (event) => {
