3ca490c7eec4b2655e6238fa2c4d3561a6906649
PhucNH <phucnh.dev@gmail.com>
2021-02-26 18:11:22 +0700

    Calculate both liq and vol on tomochain

--- src/services/index.js
+++ src/services/index.js
@@ -1,18 +1,26 @@
 import axios from 'axios'
 import BigNumber from 'bignumber.js'
 import _get from 'lodash.get'
-import { FACTORY_ADDRESS } from '../sushi/lib/constants'
+import { FACTORY_ADDRESS, FACTORY_ADDRESS_TOMOCHAIN } from '../sushi/lib/constants'
 
 const LUA_TOKEN_ADDRESS = '0xb1f66997a5760428d3a87d68b90bfe0ae64121cc'
 const route = axios.create({
   baseURL: 'https://wallet.tomochain.com/api/luaswap',
 })
-const graphRoute = axios.create({
+const graphRouteEthereum = axios.create({
   baseURL: 'https://api.thegraph.com/subgraphs/name/phucngh/luaswap',
 })
-const blockRoute = axios.create({
+const blockRouteEthereum = axios.create({
   baseURL:
     'https://api.thegraph.com/subgraphs/name/blocklytics/ethereum-blocks',
+})
+
+const graphRouteTomoChain = axios.create({
+  baseURL: 'https://api.luaswap.org/subgraphs/name/phucngh/Luaswap',
+})
+const blockRouteTomoChain = axios.create({
+  baseURL:
+    'https://api.luaswap.org/subgraphs/name/phucngh/ethereum-blocks',
 })
 
 export const getLuaPrice = (callback) => {
@@ -72,18 +80,52 @@ export const getTotalLiquidityData = (callback) => {
     }`
   const variables = {}
 
-  return graphRoute
-    .post('', {
-      operationName,
-      query,
-      variables,
+  const promiseArr = [
+    new Promise((resolve, reject) => {
+      graphRouteEthereum
+      .post('', {
+        operationName,
+        query,
+        variables,
+      }).then((res) => {
+        resolve(res)
+      }).catch((err) => {
+        reject(err)
+      })
+    }),
+    new Promise((resolve, reject) => {
+      graphRouteTomoChain
+      .post('', {
+        operationName,
+        query,
+        variables,
+      }).then((res) => {
+        resolve(res)
+      }).catch((err) => {
+        reject(err)
+      })
     })
-    .then((res) => {
-      if (_get(res, 'data.errors')) {
-        console.error('[ERROR]:', _get(res, 'data.errors.message', ''))
+  ]
+
+  return Promise.all(promiseArr)
+    .then(([resEthereum, resTomoChain]) => {
+      if (_get(resEthereum, 'data.errors')) {
+        console.error('[ERROR]:', _get(resEthereum, 'data.errors.message', ''))
         return {}
       }
-      const result = _get(res, 'data.data.uniswapFactories', []).map(
+      if (_get(resTomoChain, 'data.errors')) {
+        console.error('[ERROR]:', _get(resTomoChain, 'data.errors.message', ''))
+        return {}
+      }
+
+      const resultEther = _get(resEthereum, 'data.data.uniswapFactories', []).map(
+        (item) => ({
+          totalLiquidity: item.totalLiquidityUSD,
+          volume: item.totalVolumeUSD,
+        }),
+      )[0]
+
+      const resultTomoChain = _get(resTomoChain, 'data.data.uniswapFactories', []).map(
         (item) => ({
           totalLiquidity: item.totalLiquidityUSD,
           volume: item.totalVolumeUSD,
@@ -91,9 +133,15 @@ export const getTotalLiquidityData = (callback) => {
       )[0]
 
       if (typeof callback === 'function') {
-        return callback(result)
+        return callback({
+          totalLiquidity: new BigNumber(resultEther.totalLiquidity).plus(resultTomoChain.totalLiquidity).toString(),
+          volume: new BigNumber(resultEther.volume).plus(resultTomoChain.volume).toString()
+        })
       }
-      return result
+      return {
+        totalLiquidity: new BigNumber(resultEther.totalLiquidity).plus(resultTomoChain.totalLiquidity).toString(),
+        volume: new BigNumber(resultEther.volume).plus(resultTomoChain.volume).toString()
+      }
     })
     .catch((err) => {
       console.error('[ERROR]:', err)
@@ -101,8 +149,37 @@ export const getTotalLiquidityData = (callback) => {
     })
 }
 
-export const getBlockFromTimestamp = async (timestamp) => {
-  const result = await blockRoute.post('', {
+export const getBlockEthereumFromTimestamp = async (timestamp) => {
+  const result = await blockRouteEthereum.post('', {
+    query: `{
+      blocks(
+        first: 1
+        orderBy: timestamp
+        orderDirection: asc
+        where: {
+          ${timestamp ? `timestamp_gt: ${timestamp}` : ''}
+        }
+      ) {
+        id
+        number
+        timestamp
+      }
+    }`,
+  })
+
+  if (
+    !result ||
+    _get(result, 'data.errors') ||
+    _get(result, 'data.data.blocks.length') === 0
+  ) {
+    throw new Error('Failed to fetch block number from the subgraph.')
+  }
+
+  return _get(result, ['data', 'data', 'blocks', 0, 'number'])
+}
+
+export const getBlockTomoChainFromTimestamp = async (timestamp) => {
+  const result = await blockRouteTomoChain.post('', {
     query: `{
       blocks(
         first: 1
@@ -144,44 +221,94 @@ const getFactoryQuery = (block) => {
   }`
 }
 
+const getFactoryQueryOnTomoChain = (block) => {
+  return `query uniswapFactories {
+    uniswapFactories(
+      ${block ? `block: { number: ${block} }` : ''}
+      where: {
+        id: "${FACTORY_ADDRESS_TOMOCHAIN}"
+      }
+    ) {
+      id
+      totalVolumeUSD
+    }
+  }`
+}
+
+
+
 export const get24hVolumeChange = async () => {
   const currentTime = Math.floor(new Date().getTime() / 1000)
-  const lastDayBlockNumber = await getBlockFromTimestamp(currentTime - 86400)
-  const [volumeData, lastDayVolumeData] = await Promise.all([
-    await graphRoute.post('', {
+  const lastDayBlockNumberEthereum = await getBlockEthereumFromTimestamp(currentTime - 86400)
+  const [volumeDataEthereum, lastDayVolumeDataEtherum] = await Promise.all([
+    await graphRouteEthereum.post('', {
       query: getFactoryQuery(),
     }),
-    await graphRoute.post('', {
-      query: getFactoryQuery(lastDayBlockNumber),
+    await graphRouteEthereum.post('', {
+      query: getFactoryQuery(lastDayBlockNumberEthereum),
+    }),
+  ])
+
+  const lastDayBlockNumberTomoChain = await getBlockTomoChainFromTimestamp(currentTime - 86400)
+  const [volumeDataTomoChain, lastDayVolumeDataTomoChain] = await Promise.all([
+    await graphRouteTomoChain.post('', {
+      query: getFactoryQueryOnTomoChain(),
+    }),
+    await graphRouteTomoChain.post('', {
+      query: getFactoryQueryOnTomoChain(lastDayBlockNumberTomoChain),
     }),
   ])
 
   if (
-    !volumeData ||
-    !lastDayVolumeData ||
-    _get(volumeData, 'data.errors') ||
-    _get(lastDayVolumeData, 'data.errors')
+    !volumeDataEthereum ||
+    !lastDayVolumeDataEtherum ||
+    _get(volumeDataEthereum, 'data.errors') ||
+    _get(lastDayVolumeDataEtherum, 'data.errors')
   ) {
     throw new Error('Failed to fetch volume changes from subgraph')
   }
 
-  const currentVolume = Number(
+  if (
+    !volumeDataTomoChain ||
+    !lastDayVolumeDataTomoChain ||
+    _get(volumeDataTomoChain, 'data.errors') ||
+    _get(lastDayVolumeDataTomoChain, 'data.errors')
+  ) {
+    throw new Error('Failed to fetch volume changes from subgraph')
+  }
+
+  const currentVolumeEthereum = Number(
     _get(
-      volumeData,
+      volumeDataEthereum,
       ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
       0,
     ),
   )
-  const lastDayVolume = Number(
+  const lastDayVolumeEthereum = Number(
     _get(
-      lastDayVolumeData,
+      lastDayVolumeDataEtherum,
+      ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
+      0,
+    ),
+  )
+
+  const currentVolumeTomoChain = Number(
+    _get(
+      volumeDataTomoChain,
+      ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
+      0,
+    ),
+  )
+  const lastDayVolumeTomoChain = Number(
+    _get(
+      lastDayVolumeDataTomoChain,
       ['data', 'data', 'uniswapFactories', 0, 'totalVolumeUSD'],
       0,
     ),
   )
 
   return {
-    value: currentVolume - lastDayVolume,
-    percent: (currentVolume / lastDayVolume - 1) * 100,
+    value: (currentVolumeEthereum + currentVolumeTomoChain) - (lastDayVolumeEthereum + lastDayVolumeTomoChain),
+    percent: ((currentVolumeEthereum + currentVolumeTomoChain) / (lastDayVolumeEthereum + lastDayVolumeTomoChain) - 1) * 100,
   }
 }
