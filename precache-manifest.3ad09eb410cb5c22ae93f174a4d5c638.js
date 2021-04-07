757ac6d3e04aa983001908caf544a36b25fc1ca7
tunght91 <hoangtung.utc@gmail.com>
2021-04-07 11:45:12 +0700

    update

---
+++ precache-manifest.3ad09eb410cb5c22ae93f174a4d5c638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef86b5e20f24a8cae630fe54dddd0205",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0e862e0603a8133a96d",
+    "url": "/static/css/main.a069fb17.chunk.css"
+  },
+  {
+    "revision": "99d4124244caa3b71d23",
+    "url": "/static/js/2.3cc1c0f0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3cc1c0f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0e862e0603a8133a96d",
+    "url": "/static/js/main.d25e5410.chunk.js"
+  },
+  {
+    "revision": "ff19c8476e13aee79cd9",
+    "url": "/static/js/runtime-main.4e888963.js"
+  },
+  {
+    "revision": "d77b343b004800465f37c6a40baf4bc1",
+    "url": "/static/media/logo.d77b343b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fcdb59ee445196847ed20c040249b3e6.js"
+  "/precache-manifest.3ad09eb410cb5c22ae93f174a4d5c638.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
