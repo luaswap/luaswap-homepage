2397b58cd97ab2621c89b60971679521b3119dc8
tunght91 <hoangtung.utc@gmail.com>
2022-01-16 10:08:39 +0700

    update

---
+++ precache-manifest.3a2ad6941887b1329475863c25e1bdbc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3995dbb512d953158152cffe0a60f1f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ddad43f8582ba9cc8db",
+    "url": "/static/css/main.a069fb17.chunk.css"
+  },
+  {
+    "revision": "785c0d3e7103e50d15c1",
+    "url": "/static/js/2.dc70e5cc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.dc70e5cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ddad43f8582ba9cc8db",
+    "url": "/static/js/main.3ef2eee6.chunk.js"
+  },
+  {
+    "revision": "ff19c8476e13aee79cd9",
+    "url": "/static/js/runtime-main.4e888963.js"
+  },
+  {
+    "revision": "d77b343b004800465f37c6a40baf4bc1",
+    "url": "/static/media/logo.d77b343b.png"
+  },
+  {
+    "revision": "0fb37dc6c298f22060bb23ba2a48741f",
+    "url": "/static/media/starter.0fb37dc6.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.752e73cf1be90d8df67d150f98a60768.js"
+  "/precache-manifest.3a2ad6941887b1329475863c25e1bdbc.js"
 );
 
 self.addEventListener('message', (event) => {
